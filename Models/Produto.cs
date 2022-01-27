1316a87114d1e7e3f6e903ee5809e4a17fa46490
Ricardo Camargo Favaro <ri_ca_fa@hotmail.com>
2022-01-26 21:50:31 -0300

    first commit

---
+++ Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Range(1,20,ErrorMessage = "Valor fora do intervalo de 1 a 20")]
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
